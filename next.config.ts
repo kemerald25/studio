401a409c5220a2764bf8e7aa2f94bcaa0994d25e
Unity Ekeoba <unityekeoba18@gmail.com>
2025-10-01 14:24:50 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- next.config.ts
+++ next.config.ts
@@ -30,22 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  turbo: {
-    build: {
-      // @ts-expect-error - `onWarning` is not in the type definition
-      onWarning: (warning, context) => {
-        if(warning.message.includes('`onWarning` is not in the type definition')){
-          return
-        }
-        context.log(warning)
-      },
-    },
-    loaders: {
-      '.node': {
-        loader: 'node-loader'
-      }
-    }
-  },
   webpack: (config, {isServer}) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     if (!isServer) {
