592c0fcda12afe032a1dd6d4ae33fb41bdf8369c
Unity Ekeoba <unityekeoba18@gmail.com>
2025-10-01 15:39:21 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,7 +3,7 @@
 import { projectId, networks } from '@/config/reown';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createAppKit } from '@reown/appkit';
-import { type ReactNode } from 'react';
+import { type ReactNode, useEffect, useState } from 'react';
 import { WagmiProvider, createConfig, cookieStorage, createStorage, http } from 'wagmi';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 
@@ -34,35 +34,39 @@ const config = createConfig({
 });
 
 
-if (typeof window !== 'undefined') {
-  const wagmiAdapter = new WagmiAdapter(config);
-
-  createAppKit({
-    adapters: [wagmiAdapter],
-    projectId,
-    networks,
-    defaultNetwork: networks[0],
-    metadata: metadata,
-    enableEIP6963: true,
-    features: {
-      analytics: true,
-      email: true,
-      socials: ['google', 'x', 'discord', 'github', 'apple'],
-    },
-    allWallets: 'SHOW',
-    themeMode: 'dark',
-    themeVariables: {
-      '--w3m-accent': 'hsl(var(--primary))',
-      '--w3m-border-radius-master': 'var(--radius)',
-    },
-  });
-}
-
 export function AppProviders({
   children,
 }: {
   children: ReactNode;
 }) {
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!initialized) {
+      const wagmiAdapter = new WagmiAdapter(config);
+
+      createAppKit({
+        adapters: [wagmiAdapter],
+        projectId,
+        networks,
+        defaultNetwork: networks[0],
+        metadata: metadata,
+        enableEIP6963: true,
+        features: {
+          analytics: true,
+          email: true,
+          socials: ['google', 'x', 'discord', 'github', 'apple'],
+        },
+        allWallets: 'SHOW',
+        themeMode: 'dark',
+        themeVariables: {
+          '--w3m-accent': 'hsl(var(--primary))',
+          '--w3m-border-radius-master': 'var(--radius)',
+        },
+      });
+      setInitialized(true);
+    }
+  }, [initialized]);
 
   return (
     <WagmiProvider config={config}>
